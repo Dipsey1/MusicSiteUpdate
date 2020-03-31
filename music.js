1615b820cb9a3586427c1dfb32cf2c93a2cceead
Ginger-bread <youngdibz12@outlook.com>
2020-03-31 17:51:05 +0200

    addnew

---
+++ music.js
@@ -0,0 +1,57 @@
+const player = document.getElementById("MusicPlayer");
+console.log(player);
+function playAndPause(file) {
+    if (file != undefined) {
+        if(file != document.getElementById('song').src) {
+            document.getElementById('song').src = file;
+            player.load();
+            document.getElementById('now_playing').innerHTML = 'you are listening: ' + file + ' (click to stop)';
+            player.play();
+        } else {
+            if (player.paused) {
+                document.getElementById('now_playing').innerHTML = 'you are listening: ' + file + ' (click to stop)';
+                player.play();
+            } else {
+                document.getElementById('now_playing').innerHTML = '';
+                player.pause();
+            }
+        }
+    } else {
+        document.getElementById('now_playing').innerHTML = '';
+        player.pause();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const player = document.getElementById("MusicPlayer");
+// console.log(player);
+
+
+// function playAndPause( ) {
+//   if (player.paused) {
+//     player.play();
+//   } else {
+//     player.pause();
+//   }
+// }
+
+
